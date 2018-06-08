0790d975a9917718a65b08685cdfaff53ab189f8
Arkus <arkuscode@gmail.com>
2018-06-09 02:05:47 +0300

    Hotfix1 поправил отступы в cargo_car.rb

--- lesson7/cargo_car.rb
+++ lesson7/cargo_car.rb
@@ -1,20 +1,19 @@
 class CargoCar < Car
 
-attr_reader :filled, :type
+  attr_reader :filled, :type
 
-def initialize(capacity)
-  @capacity = capacity
-  @type = "Cargo"
-  @filled = 0
-end
+  def initialize(capacity)
+    @capacity = capacity
+    @type = "Cargo"
+    @filled = 0
+  end
 
-def load(volume)
-  raise "Свободное место закончилось!" if @filled + volume > @capacity
-  @filled += volume
-end
+  def load(volume)
+    raise "Свободное место закончилось!" if @filled + volume > @capacity
+    @filled += volume
+  end
 
-def free
-  @capacity - @filled
-end
-
+  def free
+    @capacity - @filled
+  end
 end
